9c33fb7cff590edd44505ca8365c42026a92429b
tlaJack <jack@tlagraphics.com>
2021-03-05 15:36:31 -0500

    add sublink funcitonality - in progress

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,30 @@
 import React from "react";
-// import { Link } from "gatsby";
+import { Link } from "gatsby";
+
+const navlinks = [
+  { name: "home", slug: "/" },
+  { name: "about", slug: "/about" },
+  {
+    name: "item1",
+    slug: "/item1",
+    sublink: [
+      { name: "item2", slug: "/item2" },
+      { name: "item3", slug: "/item3" },
+    ],
+  },
+];
 
 const Header = () => {
   return (
     <>
-      <p>header</p>
+      <ul>
+        {navlinks.map((navlink, i) => (
+          <li>
+            <Link to={navlinks[i].slug}>{navlinks[i].name}</Link>
+            {navlinks[i].sublink ? <p>sublink</p> : null}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
